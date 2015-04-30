614bc00ac52c10683cabd0fc670400f7948cf6d6
Milad Khajvi <khajavi@gmail.com>
2015-04-30 12:43:48 +0430

    add deploy.sh script.

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project. 
+hugo -t hugo-rtl
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -aSs -m "$msg"
+
+# Push source and build repos.
+git push origin hugo
+git subtree push --prefix=public git@github.com:khajavi/khajavi.github.io.git master
